fc06db77f1080e87b7c2ea1523b515aeefb155cc
obada <obadaquraan@yahoo.com>
2019-11-26 06:06:15 +0200

    fix

--- server.js
+++ server.js
@@ -2,94 +2,71 @@
 
 require('dotenv').config();
 const express = require('express');
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 const superagent=require('superagent');
+
 app.use( express.json() );
-
-//
-
 app.use( express.urlencoded({extended:true}));
 app.use( express.static('./public/../') );
 app.set('view engine' ,'ejs')
 
-
+// routes 
 app.get('/', (req,res) => {
 res.render('pages/index') ;
  });
 
-app.get('/show', (req,res) => {
-    res.render('pages/searches/show') ;
-});
+ app.get('/show', (req,res) => {
+  // let people=['obada','fooo']
+  
+     res.render('pages/searches/show') ;
+ });
 
-app.post('/books',bookHandler)
-
-//new search page 
-app.get('/search',(request, response)=>{
+ app.get('/search',(request, response)=>{
+  // console.log('re : ', request.query);
+  
   response.render('pages/searches/show');
 });
 
-app.get('/search', postSearch);
-
-function bookHandler(req,res){
-  getBook(req.query.data)
-  .then((booksData) => res.status(200).json(booksData));
-}
-
-
-function getBook() {
-  
-  const url =`https://www.googleapis.com/books/v1/volumes?q=tree`
+app.post('/search',(request,response)=>{
+  console.log(request.body.select)
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if(request.body.select === 'author') {url += `inauthor:${request.body.name}&maxResults=10`;}
+  if(request.body.select === 'title') {url += `intitle:${request.body.name}&maxResults=10`;}
 
   return superagent.get(url)
     .then(data => {
-      let newBook = new Book(data.body)
-    
+      
+      let newBook= data.body.items.map((item)=>{
+       return new Book(item)
+      })
       return newBook;
     })
-   
-    
-}
-
-
-
-function postSearch(request, response){
-  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  if(request.body.search[1] === 'author') {url += `inauthor:${request.body.search[0]}&maxResults=10`;}
-  if(request.body.search[1] === 'title') {url += `intitle:${request.body.search[0]}&maxResults=10`;}
-  superagent.get(url)
-
-    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => {
-
-      response.render('pages/searches/showSearchResults', {searchResults: results});
-
-    })
-
-    .catch(error => {
-      errorHandle(error, response);
-    });
-}
-
-
+  .then((booksData) => {
+   response.render('pages/searches/show',{data:booksData})
+  
+  });
+  
+})
 
 function Book(data){
-  this.title=data.items[0].volumeInfo.title;
-  this.authors=data.items[0].volumeInfo.authors[0];
-  this.id=data.items[0].id;
-  this.url=data.items[0].volumeInfo.imageLinks.thumbnail;
-  this.description=data.items[0].volumeInfo.description;
+  this.title=data.volumeInfo.title;
+  this.authors=data.volumeInfo.authors &&data.volumeInfo.authors[0];
+  this.isbn=data.volumeInfo.industryIdentifiers&&data.volumeInfo.industryIdentifiers[0].identifier;
+  this.url=data.volumeInfo.imageLinks&&data.volumeInfo.imageLinks.thumbnail;
+  this.description=data.volumeInfo.description;
 }
 
 function notFound(req,res){
   res.status(404).send('NOT FOUND!!!');
 }
-
 function errorHandler(error,req,res){
-
   res.status(500).send(error);
 }
 
 
 app.listen( PORT, () => console.log(`Up on port ${PORT}`));
+
+
+
+
